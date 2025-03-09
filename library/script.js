5893ffe05b0b3534db24f7743f1fd8c55205dc5e
Thongqi <141812298+Thongqi@users.noreply.github.com>
2025-03-09 13:38:59 +0800

    construtor opreation change from , to ;

--- library/script.js
+++ library/script.js
@@ -29,11 +29,11 @@ addBooktoLibrary(
 )
 
 function Book(title, author, pages, read){
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read,
-    this.id = crypto.randomUUID(),
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
 }
 
 function addBooktoLibrary(title, author, pages, read){
