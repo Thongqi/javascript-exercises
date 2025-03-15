4696b8dd3c827b02e6fb97aa83e57ba279b9ec1c
Thongqi <141812298+Thongqi@users.noreply.github.com>
2025-03-15 14:20:49 +0800

    add in checkwin func

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -4,21 +4,27 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     
-    console.log ({user, comp})
-    var select = document.querySelectorAll('input[name="user-select"]')
+    console.log ({user, comp});
+    var select = document.querySelectorAll('input[name="user-select"]');
 
-    storeSelect(select)
+    storeSelect(select);
     
 
 })
 
 function userclick(e){
     //check if the grid is empty
-    console.log(e)
+    console.log(e);
+
+    // change dom
     if(e.innerHTML == ''){
-        console.log(user.selected)
-        e.innerHTML = (user.selected=='cross')?'x':'o'
+        console.log(user.selected);
+        e.innerHTML = (user.selected=='cross')?'x':'o';
+        e.classList.add(user.selected);
     }
+
+    // check if win
+    checkWin()
 }
 
 const createUser = function(){
@@ -44,7 +50,7 @@ function storeSelect(select){
            if (e.target.checked){
                user.selected = e.target.getAttribute('data-selected');
                comp.selected = e.target.getAttribute('data-selected')=='cross'?'circle':'cross';
-               console.log({select: user.selected, comp: comp.selected})
+               console.log({select: user.selected, comp: comp.selected});
            }
        })
    })
@@ -52,7 +58,22 @@ function storeSelect(select){
 
 //check if a line
 function checkWin(){
-    const container = document.querySelector('.container')
+    const container = document.querySelector('.container');
+
+    let grids = container.querySelectorAll(`.${user.selected}]`);
+    let gridnumbers;
+    grids.map(item => gridnumbers.append(item.className));
+
+    console.log(gridnumbers.map(item => item.slice(5)));
+
+    gridnumbers.sort();
+    if (gridnumbers.length >= 3){
+        let result = gridnumbers.filter(v1 => winCondition.includes(v1));
+        console.log(result.length >= 3);
+        if (result.length >= 3){
+            alert ('You Win!')
+        }
+    }
 
 
 }
@@ -60,4 +81,13 @@ function checkWin(){
 
 
 // difference between location of grid is 1 (with condition the lowest number shall be {1,4,7})/3/4, when 3 x 3
-const winCondition = [1, 3, 4]
+const winCondition = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9,],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [3,5,7],
+]
