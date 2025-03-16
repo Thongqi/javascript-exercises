9e97367b831361d2778b3041bcfba1c686366217
Thongqi <141812298+Thongqi@users.noreply.github.com>
2025-03-16 09:54:23 +0800

    try to block opponent from winning

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -130,17 +130,54 @@ function announceWinner(player){
 function compPlay(){
     var container = document.querySelector('.container')
     var availablegrid = Array.from(container.querySelectorAll('div'));
+    var oppgrid = Array.from(container.querySelectorAll(`.${user.select}`));
 
     availablegrid = availablegrid.filter(item => item.classList.length == '0');
+    availablegrid = availablegrid.map(item => item.id.slice(5));
 
-    var index = Math.floor(Math.random() * availablegrid.length);
 
-    index = availablegrid[index].id.slice(5)
+    // first move if can, choose from 1,3,7,9
+    var evengrid = availablegrid.filter(item => item % 2 != '0');
+    console.log(evengrid)
+    if(oppgrid.length <= 2){
+        var index = Math.floor(Math.random() * evengrid.length);
 
+        index = availablegrid[index];
+    }
+    else{
+        var index = blockOpp(availablegrid. oppgrid)
+
+        if(!index){
+            var index = Math.floor(Math.random() * availablegrid.length);
+
+            index = availablegrid[index];
+        }
+       
+    }
     // document.querySelector(`#cont-${index}`).innerHTML = comp.selected=='circle'?'o':'x';
     document.querySelector(`#cont-${index}`).classList.add(comp.selected);
 
     checkWin(comp)
+}
+
+function blockOpp(availablegrid, oppgrid){
+    oppgrid = oppgrid.map(item => item.id.slice(5));
+
+    for(set of winCondition){
+        let result = set.filter(i => oppgrid.includes(i))
+        if(result.length >= 2){
+            let value = set.filter(i => !oppgrid.includes(i));
+            console.log(result, value)
+            // check if the grid is empty
+            if(!availablegrid.includes(value[0])){
+                return value;
+                break;
+            }
+        }
+        return false;
+    }
+
+
 }
 
 
@@ -148,7 +185,7 @@ function compPlay(){
 const winCondition = [
     [1,2,3],
     [4,5,6],
-    [7,8,9,],
+    [7,8,9],
     [1,4,7],
     [2,5,8],
     [3,6,9],
